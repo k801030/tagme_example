6c3e7a70f555a2aab42c44e9fdc0f5647ee0334b
Vison <kk801030@gmail.com>
2015-01-08 17:46:47 +0800

    3-step sign-up form

---
+++ public/js/form.js
@@ -0,0 +1,31 @@
+angular.module('formApp', ['ui.router'])
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    //nested states
+    .state('yourtags', {
+      url: '/sign-up',
+      templateUrl: '/form/yourtags'
+    })
+
+    .state('mytags', {
+      templateUrl: '/form/mytags'
+    })
+
+    .state('contact', {
+      templateUrl: '/form/contact'
+    })
+
+    $urlRouterProvider.otherwise('/sign-up');
+})
+
+.controller('formController', ['$scope', function($scope){
+  $scope.formData = {};
+  $scope.formData.yourtags = [];
+  $scope.formData.mytags = [];
+  $scope.formData.contact = {};
+  $scope.signUp = function() {
+
+  };
+}])
